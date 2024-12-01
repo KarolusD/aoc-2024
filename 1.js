7a841b2aba262cbc37cf8d2108d08dfe95d2604c
KarolusD <kpodzerek@gmail.com>
2024-12-01 17:41:56 +0100

    day 1 - clean up

--- 1.js
+++ 1.js
@@ -14,58 +14,48 @@ function day1() {
       right.push(Number(distances[1]));
     });
 
-  const distance = totalDistance(left.slice(), right.slice());
-  const similarity = countSimilarityScore(left, right);
+  const distance = totalDistance(left.sort(), right.sort());
+  const similarity = countSimilarityScore(left.sort(), right.sort());
 
   return { distance, similarity };
 }
 
-function totalDistance(left, right, total = 0) {
-  if (left.length === 0 || right.length === 0) {
-    return total;
-  }
+function totalDistance(left, right) {
+  return left.map((l, idx) => Math.abs(l - right[idx])).reduce((a, b) => a + b);
 
-  const ls = findSmallest(left);
-  const rs = findSmallest(right);
-  const diff = Math.abs(ls.value - rs.value);
+  // Without Sorting 🤷
+  // if (left.length === 0 || right.length === 0) {
+  //   return total;
+  // }
+  //
+  // const ls = findSmallest(left);
+  // const rs = findSmallest(right);
 
-  left.splice(ls.pos, 1);
-  right.splice(rs.pos, 1);
-
-  return totalDistance(left, right, total + diff);
+  // return totalDistance(left, right, total + diff);
 }
 
 function countSimilarityScore(left, right) {
-  let total = 0;
-
-  left.forEach((l) => {
-    let times = 0;
-    right.forEach((r) => {
-      if (l === r) {
-        times++;
-      }
-    });
-    total += l * times;
-  });
-  return total;
+  return left
+    .map((l) => right.filter((r) => r === l).length * l)
+    .reduce((a, b) => a + b);
 }
 
 // returns value and the position of smallest number in the array
-function findSmallest(list) {
-  if (list.length === 0) {
-    console.log("List is empty");
-    return 0;
-  }
-
-  return list.reduce(
-    (acc, curr, idx) => {
-      if (curr < acc.value) {
-        return { value: curr, pos: idx };
-      }
-      return acc;
-    },
-    { value: list[0], pos: 0 },
-  );
-}
+// function findSmallest(list) {
+//   if (list.length === 0) {
+//     console.log("List is empty");
+//     return 0;
+//   }
+//
+//   return list.reduce(
+//     (acc, curr, idx) => {
+//       if (curr < acc.value) {
+//         return { value: curr, pos: idx };
+//       }
+//       return acc;
+//     },
+//     { value: list[0], pos: 0 },
+//   );
+// }
 
 console.log(day1());
